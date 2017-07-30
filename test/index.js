6878ec633a8885b797a550eb9ca058f4d83ae5f4
Matti Jokitulppo <melonmanchan@gmail.com>
2017-07-30 14:53:54 +0200

    Add test for chrome and firefox

--- test/index.js
+++ test/index.js
@@ -2,11 +2,15 @@ import assert from 'assert'
 
 import canIUseMiddleWare from '../src'
 import features from './fixtures/features'
+import chrome from './fixtures/chrome'
+import firefox from './fixtures/firefox'
 
 const res = {}
 const next = () => {}
 
 const middleWare = canIUseMiddleWare({ features })
+
+const getReq = (ua) => ({ headers: { 'user-agent': ua } })
 
 describe('canIUseMiddleWare', function () {
   it('should return empty object as capabilities if no user-agent in request', () => {
@@ -18,12 +22,26 @@ describe('canIUseMiddleWare', function () {
   })
 
   it('should return empty object as capabilities if unknown user-agent', () => {
-    const req = { headers: {
-      'user-agent': 'Some mysterious user-agent'
-    } }
+    const req = getReq('Some mysterious user-agent')
 
     middleWare(req, res, next)
 
     assert.deepEqual(req, Object.assign({}, req, {capabilities: {}}))
+  })
+
+  it('parses chrome user-agent and features correctly', () => {
+    const req = getReq(chrome.ua)
+
+    middleWare(req, res, next)
+
+    assert.deepEqual(req.capabilities, chrome.capabilities)
+  })
+
+  it('parses firefox user-agent and features correctly', () => {
+    const req = getReq(firefox.ua)
+
+    middleWare(req, res, next)
+
+    assert.deepEqual(req.capabilities, firefox.capabilities)
   })
 })
