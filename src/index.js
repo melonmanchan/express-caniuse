18bd8654914dd108dfde55dc123f9c105d22363a
Matti Jokitulppo <melonmanchan@gmail.com>
2017-07-30 12:12:05 +0200

    Remove testing stuff

--- src/index.js
+++ src/index.js
@@ -48,16 +48,3 @@ export default function canIUseMiddleWare (opts = {}) {
     next()
   }
 }
-
-canIUseMiddleWare({ features: [
-  'border-radius',
-  'css3-cursors'
-]})(
-  {
-    headers: {
-      'user-agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
-    }
-  },
-  {},
-  () => {}
-)
